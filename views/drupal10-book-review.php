4b6fe1e2997794abbbb5fa64b8c3837cf0209890
Eugene Lefter <eugen.lefter@gmail.com>
2024-02-29 16:40:59 -0500

    book review head update

--- views/drupal10-book-review.php
+++ views/drupal10-book-review.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'layouts/head/head_article_drupal7.php'; ?>
-
-
-
-
+<?php include 'layouts/head/head_article_drupal_review.php'; ?>
 
 <body>
 
---
+++ views/layouts/head/head_article_drupal_review.php
@@ -0,0 +1,32 @@
+<head>
+	<title>Book Review: Drupal 10 Masterclass - VMNBY Tech Digital Agency</title>
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="vmnbytech.com" />
+	<meta name="title" content="Book Review: Drupal 10 Masterclass by Adam Bergstein." />
+	<meta name="description" content="Book Review: Drupal 10 Masterclass by Adam Bergstein. this book is an indispensable resource for anyone looking to learn or deepen their knowledge of Drupal CMS.">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+
+	<!-- Favicon -->
+	<meta name="title" property="og:title" content="Book Review: Drupal 10 Masterclass by Adam Bergstein" />
+	<meta name="description" property="og:description" content="Book Review: Drupal 10 Masterclass by Adam Bergstein. this book is an indispensable resource for anyone looking to learn or deepen their knowledge of Drupal CMS." />
+	<meta property="og:url" content="https://vmnbytech.com/drupal10-book-review" />
+	<meta property="og:image" content="https://vmnbytech.com/assets/images/blog/drupal10.jpg" />
+	<link rel="canonical" href="https://vmnbytech.com/drupal10-book-review/" />
+	<link rel="alternate" hreflang="x-default" href="https://vmnbytech.com/drupal10-book-review/" />
+	<link rel="shortcut icon" href="assets/images/favicon.png">
+
+	<link href="assets/css/font.css" rel="stylesheet" />
+
+	<!-- Plugins CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/vendor/font-awesome/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap-icons/bootstrap-icons.css">
+	<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.min.css">
+
+	<!-- Theme CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script async src="assets/js/gaAnalytics.js"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-42RFV7HHWV"></script>
+</head>
