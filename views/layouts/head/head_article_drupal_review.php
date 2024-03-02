1eec75ef7f3c669e8d34db83f1e5808398926f54
Eugene Lefter <eugen.lefter@gmail.com>
2024-03-02 13:20:41 -0500

    cookie alert update

--- views/layouts/head/head_article_drupal_review.php
+++ views/layouts/head/head_article_drupal_review.php
@@ -5,13 +5,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="author" content="vmnbytech.com" />
 	<meta name="title" content="Book Review: Drupal 10 Masterclass by Adam Bergstein." />
-	<meta name="description" content="Book Review: Drupal 10 Masterclass by Adam Bergstein. This book is an indispensable resource for anyone looking to learn or deepen their knowledge of Drupal CMS.">
+	<meta name="description"
+		content="Book Review: Drupal 10 Masterclass by Adam Bergstein. This book is an indispensable resource for anyone looking to learn or deepen their knowledge of Drupal CMS.">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 
 	<!-- Favicon -->
 	<meta name="title" property="og:title" content="Book Review: Drupal 10 Masterclass by Adam Bergstein" />
-	<meta property="og:description" content="Book Review: Drupal 10 Masterclass by Adam Bergstein. This book is an indispensable resource for anyone looking to learn or deepen their knowledge of Drupal CMS." />
+	<meta property="og:description"
+		content="Book Review: Drupal 10 Masterclass by Adam Bergstein. This book is an indispensable resource for anyone looking to learn or deepen their knowledge of Drupal CMS." />
 	<meta property="og:url" content="https://vmnbytech.com/drupal10-book-review" />
 	<meta property="og:image" content="https://vmnbytech.com/assets/images/blog/drupal10-432x533x.png" />
 	<link rel="canonical" href="https://vmnbytech.com/drupal10-book-review/" />
